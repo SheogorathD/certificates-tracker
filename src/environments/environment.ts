245e469ad840e596ef262ac2329a720f6336f8d0
Volodymyr Shcherbyna <aknojlogiya@gmail.com>
2023-02-06 11:50:33 +0200

    Real Initial Commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  firebase: {
+    projectId: 'shipscertificatestracker',
+    appId: '1:722996939245:web:37e1b401ec40ebab613fe4',
+    storageBucket: 'shipscertificatestracker.appspot.com',
+    apiKey: 'AIzaSyA-zWzyAFiPu9Yf2_y6lLt-Rez4ydayqNM',
+    authDomain: 'shipscertificatestracker.firebaseapp.com',
+    messagingSenderId: '722996939245',
+  },};
